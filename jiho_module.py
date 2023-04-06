850099a62fa3dc6fcd9b3dab2407604387f9a847
jihosuperman <jiho951127@gmail.com>
2023-04-06 16:37:17 +0900

    replace the dask code with spark again

--- jiho_module.py
+++ jiho_module.py
@@ -1,17 +1,15 @@
-import dask as dd
+import dask.dataframe as dd
+from dask.dataframe import concat
 
 def concat_and_check(df1, df2):
-    df_new = dd.concat([df1, df2]).reset_index(drop=True)
+    df_new = concat([df1, df2]).reset_index(drop=True)
     check = True
     if len(df1) + len(df2) == len(df_new):
         check = True
     else:
         check = False
-
     print(check)
     return df_new
-
-
 
 def active_day_per_total(log_data):
     user_active_count = log_data.groupBy("msno").count()
