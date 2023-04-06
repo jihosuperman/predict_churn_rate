bd095e8e43dae67756079a28ad41c149e60df0b3
jihosuperman <jiho951127@gmail.com>
2023-04-07 01:40:03 +0900

    rename main --> EDA and testing the calculate total periods of user paid for Service

--- jiho_module.py
+++ jiho_module.py
@@ -1,2 +1,21 @@
-def active_day_per_total(log_data):
-    user_active_count = log_data.groupBy("msno").count()
+from pyspark.sql.functions import to_date, substring, concat_ws
+from pyspark.sql import DataFrame
+
+
+def active_day_per_total(transaction_df):
+    transaction_df = (transaction_df
+                       .withColumn('transaction_date', 
+                                   concat_ws('-', substring(transaction_df['transaction_date'], 1, 4), substring(transaction_df['transaction_date'], 5, 2), substring(transaction_df['transaction_date'], 7, 2)))
+                    )
+    transaction_df = (transaction_df
+                       .withColumn('transaction_date', to_date(transaction_df['transaction_date'], 'yyyy-MM-dd'))
+                    )
+    transaction_df = (transaction_df
+                       .withColumn('membership_expire_date', 
+                                   concat_ws('-', substring(transaction_df['membership_expire_date'], 1, 4), substring(transaction_df['membership_expire_date'], 5, 2), substring(transaction_df['membership_expire_date'], 7, 2)))
+                    )
+    transaction_df = (transaction_df
+                       .withColumn('membership_expire_date', to_date(transaction_df['membership_expire_date'], 'yyyy-MM-dd'))
+                    )
+    
+    return transaction_df
