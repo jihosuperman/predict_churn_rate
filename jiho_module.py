8131d8e3a6f72e537152d0cfe4bf1e6a77592798
jihosuperman <jiho951127@gmail.com>
2023-04-10 01:10:48 +0900

    modify first function to int_to_date()

--- jiho_module.py
+++ jiho_module.py
@@ -1,37 +1,60 @@
 from pyspark.sql.functions import to_date, substring, concat_ws
 from pyspark.sql import DataFrame
 import pandas as pd
+from functools import reduce
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 
-def active_day_per_total(transaction_df):
+def int_to_date(transaction_df, col_name):
     transaction_df = (transaction_df
-                       .withColumn('transaction_date', 
-                                   concat_ws('-', substring(transaction_df['transaction_date'], 1, 4), substring(transaction_df['transaction_date'], 5, 2), substring(transaction_df['transaction_date'], 7, 2)))
+                       .withColumn(
+                            col_name, 
+                            concat_ws(
+                                '-', 
+                                substring(transaction_df[col_name], 1, 4), 
+                                substring(transaction_df[col_name], 5, 2), 
+                                substring(transaction_df[col_name], 7, 2)))
                     )
     transaction_df = (transaction_df
-                       .withColumn('transaction_date', to_date(transaction_df['transaction_date'], 'yyyy-MM-dd'))
-                    )
-    transaction_df = (transaction_df
-                       .withColumn('membership_expire_date', 
-                                   concat_ws('-', substring(transaction_df['membership_expire_date'], 1, 4), substring(transaction_df['membership_expire_date'], 5, 2), substring(transaction_df['membership_expire_date'], 7, 2)))
-                    )
-    transaction_df = (transaction_df
-                       .withColumn('membership_expire_date', to_date(transaction_df['membership_expire_date'], 'yyyy-MM-dd'))
+                       .withColumn(
+                            col_name, 
+                            to_date(
+                                transaction_df[col_name], 
+                                'yyyy-MM-dd'))
                     )
     
     return transaction_df
 
-def multi_join_left(df1, df2, df3, df4):
-    total_df = df1.join(df2, on='msno', how='left').join(df3, on='msno', how='left').join(df4, on='msno', how='left')
-    
-    return total_df
+def join_dataframes(dataframes: list, join_condition: str) -> DataFrame:
+    """
+    Takes a list of Spark DataFrames and a join condition, and performs an inner join on all of them.
+    """
+    return reduce(lambda df1, df2: df1.join(df2, on='msno',how=join_condition), dataframes)
 
-def multi_join_inner(*args):
-    for df in args:
-      total_df = df1.join(df2, on='msno', how='inner').join(df3, on='msno', how='inner').join(df4, on='msno', how='inner')
-    
-    return total_df
-
-def sparkDF_to_csv(df, address):
-    df.to_Pandas().to_csv(address)
+def sparkDF_to_csv(df: DataFrame, address: str):
+    """
+    Take a Spark DataFrame and export it as csv format.
+    """
+    df.toPandas().to_csv(address)
     print('Finish!!')
+
+def print_model_result(model, X_test, y_test):
+   pred_test2 = model.predict(X_test)
+
+   print("accuracy:", accuracy_score(y_test, pred_test2))
+   print("precision:", precision_score(y_test, pred_test2))
+   print("recall:", recall_score(y_test, pred_test2))
+   print("f1 score:", f1_score(y_test, pred_test2))
+   print(confusion_matrix(y_test, pred_test2, labels=[1,0]))
+
+def print_grid_result(model, X_test, y_test):
+   print('best parameters : ', model.best_params_)
+   print('best score : ', model.best_score_)
+   best_test2 = model.best_estimator_
+   pred_test2 = model.predict(X_test)
+
+   print("accuracy:", accuracy_score(y_test, pred_test2))
+   print("precision:", precision_score(y_test, pred_test2))
+   print("recall:", recall_score(y_test, pred_test2))
+   print("f1 score:", f1_score(y_test, pred_test2))
+   print(confusion_matrix(y_test, pred_test2))
